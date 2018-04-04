90131a8f627f6be8c4aca9e79c8e7d3d83798bb7
braedonww@gmail.com <braedonww@gmail.com>
2018-04-04 17:14:41 +1000

    Tests

--- build.zig
+++ build.zig
@@ -2,11 +2,6 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: &Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("zigJSON", "src/json.zig");
-    exe.setBuildMode(mode);
-
-    b.default_step.dependOn(&exe.step);
-    b.installArtifact(exe);
     const test_step = b.step("test", "Run all the tests");
     const tests = b.addTest("tests/test.zig");
     tests.setBuildMode(mode);
